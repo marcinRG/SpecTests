529579d8f203c9e251e445d42e2e0844882753bf
marcinRG <marcin.guszpit@gmail.com>
2020-02-17 00:14:48 +0100

    init

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import style from './app.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <React.Fragment>
+    </React.Fragment>,
+    document.getElementById('App'));
