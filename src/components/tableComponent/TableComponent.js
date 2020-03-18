a3b55f637f9c8fef07803a88d232cb3c6735869a
marcinRG <marcin.guszpit@gmail.com>
2020-03-18 07:20:21 +0100

    changes

--- src/components/tableComponent/TableComponent.js
+++ src/components/tableComponent/TableComponent.js
@@ -1,23 +1,53 @@
 import React, {Component} from 'react';
 import {TableLabelComponent} from './tableLabelComponent/TableLabelComponent';
 import {TableRowComponent} from './tableRowComponent/TableRowComponent';
+import {LabelComponent} from './tableLabelComponent/LabelComponent';
 
 
 export class TableComponent extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: [
+                {
+                    date: '2020-03-17T11:32:38.000Z',
+                    document_nr: '203455/30',
+                    document_sum: 2457.12
+                },
+                {
+                    date: '2020-03-17T11:32:38.000Z',
+                    document_nr: '203455/31',
+                    document_sum: 210.77
+                },
+                {
+                    date: '2020-03-17T11:32:38.000Z',
+                    document_nr: '203455/32',
+                    document_sum: 5545.66
+                },
+                {
+                    date: '2019-12-17T11:32:38.000Z',
+                    document_nr: '203455/95',
+                    document_sum: 125.30
+                },
+                {
+                    date: '2020-02-20T11:32:38.000Z',
+                    document_nr: '203455/12',
+                    document_sum: 8825.73
+                }
+            ]
+        }
     }
 
     render() {
         return (
             <table className="item-list-table">
-                <TableLabelComponent />
+                <TableLabelComponent/>
                 <tbody>
-                  <TableRowComponent/>
-                  <TableRowComponent/>
-                  <TableRowComponent/>
-                  <TableRowComponent/>
-                  <TableRowComponent/>
+                {this.state.data.map((document, i) =>
+                    <TableRowComponent key={i} date={document.date} number={document.document_nr}
+                      total={document.document_sum} />
+                    )
+                }
                 </tbody>
             </table>
         )
--- src/components/tableComponent/tableRowComponent/TableRowComponent.js
+++ src/components/tableComponent/tableRowComponent/TableRowComponent.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 export class TableRowComponent extends Component {
     constructor(props) {
@@ -8,11 +9,34 @@ export class TableRowComponent extends Component {
     render() {
         return (
             <tr>
-                <td className="table-cell">Name</td>
-                <td className="table-cell">Name</td>
-                <td className="table-cell">Name</td>
-                <td className="table-cell">Name</td>
+                <td className="table-cell">{getDateAsString(this.props.date,'-')}</td>
+                <td className="table-cell">{this.props.number}</td>
+                <td className="table-cell">{this.props.total}</td>
+                <td className="table-cell"><button className="table-button">Edit</button>
+                    <button className="table-button">Remove</button></td>
             </tr>
         );
     }
 }
+
+function getDateAsString(dateAsString, separator) {
+    const date = new Date(dateAsString);
+    return `${date.getFullYear()}${separator}${formatNumbers(date.getMonth())}${separator}${formatNumbers(date.getDay())}`;
+}
+
+function validateDate(dateAsString) {
+    return (dateAsString && Date.parse(dateAsString) !== Number.NaN);
+}
+
+function getDateFromString(dateAsString) {
+    if (validateDate(dateAsString)) {
+        return new Date(dateAsString);
+    }
+}
+
+
+TableRowComponent.propTypes = {
+    date: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+    total: PropTypes.number.isRequired
+};
